d0ff1713da1571c1e928364fe6b37c4e15da046d
Tamara Bozhanova <tamara.bozhanova@gmail.com>
2018-07-09 11:20:22 +0300

    initial commit

---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,50 @@
+class PhonesController < ApplicationController
+
+  before_action :set_phone, only: [ :show, :edit, :update, :destroy]
+
+  def index
+    @phones =Phone.all
+  end
+
+  def show
+  end
+
+  def new
+    @phone=Phone.new
+  end
+
+  def create
+    @phone=Phone.new(phone_params)
+    if @phone.save
+      redirect_to @phone, success: 'Телефон успешно создан'
+    else
+      render :new, danger: 'Телефон не создан'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @phone.update_attributes(phone_params)
+      redirect_to @phone, success: 'Телефон успешно обновлен'
+    else
+      render :edit, danger: 'Телефон не обновлен'
+    end
+  end
+
+  def destroy
+    @phone.destroy
+    redirect_to phones_path, success: 'Телефон успешно удален'
+  end
+
+  private
+
+  def set_phone
+    @phone=Phone.find(params[:id])
+  end
+
+    def phone_params
+      params.require(:phone).permit(:name, :price, :description, :image)
+    end
+end
---
+++ app/models/phone.rb
@@ -0,0 +1,5 @@
+class Phone < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  validates :name, :price, :description, presence: true
+  validates :price, numericality: {greater_than: 0, allow_nil: true}
+end
